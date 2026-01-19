66b988a92dff51e26a0d22a6d285794b056f50ff
KaiStow <kaistow7@gmail.com>
2026-01-19 09:28:34 +0000

    project complete, AI is now called and a summary is written to a file

--- TranscriptAgent/main.py
+++ TranscriptAgent/main.py
@@ -1,16 +1,23 @@
 from record import Record
 from transcribe import Transcriber
+from summary import AISummary
+from dotenv import load_dotenv
+load_dotenv()
 
 class Main:
     def __init__(self):
         self.recorder = Record()
         self.transcriber = Transcriber()
+        self.summary = AISummary()
 
     def run(self):
         self.recorder.record()
         print("Recording process completed.")
         print("Transcription process starting.")
         self.transcriber.transcribe_to_file()
+        print("Transcription process completed.")
+        print("Summary process starting.")
+        self.summary.summarize()
 
 main = Main()
 main.run()
--- TranscriptAgent/summary.py
+++ TranscriptAgent/summary.py
@@ -1,7 +1,6 @@
 from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_classic.agents import create_tool_calling_agent, AgentExecutor
-from tools import save_tool
 
 class AISummary:
     def __init__(self, transcript = "transcript.txt"):
@@ -11,16 +10,31 @@ class AISummary:
     def summarize(self):
         with open(self.transcript, 'r') as f:
             self.content = f.read()
+            self.text = ("".join(self.content))
             self.generate_prompt()
             self.call_AI()
+            self.write_to_file()
         
     def generate_prompt(self):
         self.prompt = ChatPromptTemplate.from_messages(
             [
-                ("system", "You are an expert summarization AI. Provide concise and informative summaries. The given transcript is from a recorded conversation that may contain informal language and filler words. Focus on extracting key points, main ideas, and relevant details while omitting unnecessary filler content."),
-                ("user", f"Summarize the following transcript:\n{self.content}")
+                ("system", "You are an expert summarization AI. Provide concise and informative summaries. The query is a raw transcript from a recorded conversation that may contain informal language and filler words. Focus on extracting key points, main ideas, and relevant details while omitting unnecessary filler content."),
+                ("placeholder", "{chat_history}"),
+                ("human", "{query}"),
+                ("placeholder", "{agent_scratchpad}"),
             ]
         )
+
+    def parse_response(self):
+        output = self.raw_response['output']
+        text_response = output[0]['text']
+        return text_response
+
+    def write_to_file(self, file = "summary.txt"):
+        self.summary = self.parse_response()
+        with open(file, 'w') as f:
+            f.write(self.summary)
+        print(f"Summary saved to {file}")
 
     def call_AI(self):
         self.agent = create_tool_calling_agent(
@@ -33,5 +47,6 @@ class AISummary:
             tools=[],
             verbose=True
         )
-        raw_response = self.executor.run(self.prompt)
-        print(raw_response)
+        self.raw_response = self.executor.invoke({"query": self.text})
+        print(f"raw response: {self.raw_response}")
+        print("parsing response...")
